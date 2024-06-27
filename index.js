f18925a218f8fb5b2a8bcf8157b80d26fabf9c93
zahrafeisal <zahrafeisal3@gmail.com>
2024-06-27 17:28:06 +0300

    removed main element and added h1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.querySelector("main#main");
+// console.log(main);
+// main.remove();
+console.log(main.remove());
+
+const newHeader = document.createElement("h1");
+console.log(newHeader);
+
+newHeader.id = "victory";
+console.log(newHeader);
+
+newHeader.textContent = "Zahra is the champion";
+console.log(newHeader);
